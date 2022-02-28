3ccd753f94069234c6f00092e972c4499382a931
Diogo <diogosantosferreira.01@gmail.com>
2022-02-28 19:38:00 -0300

    materias

--- materias/urls.py
+++ materias/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Materias
+from .views import Materias, MateriaById
 
 urlpatterns = [
-    path('materias/', Materias.as_view())
+    path('materias/', Materias.as_view()),
+    path('materias/<int:materia_id>', MateriaById.as_view())
+    
 ]
--- materias/views.py
+++ materias/views.py
@@ -37,6 +37,50 @@ class Materias(APIView):
             return {'Ainda não foi registrado nenhuma materia'}, 204
 
 
+class MateriaById(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [Instructor]
+
+    def put(self, request, materia_id):
+        try:
+            materia = RegisterMaterias.objects.get(id=materia_id)
+            name_materia = request.data['name_materia']
+            materia.name_materia = name_materia
+            materia.save()
+            serializer = MateriasSerializer(materia)
+
+            return Response(serializer.data, status=200)
+            
+        except KeyError as e:
+            return Response({"error": f"{str(e)} is missing"}, status=400)
+
+        except RegisterMaterias.DoesNotExist:
+            return Response({'errors': 'invalid materia_id'}, status=404)
+
+        except IntegrityError:
+            return Response({"error": "Materia with this name already exists"}, status=400)
+
+
+    def get(self, request, materia_id):
+        try:
+            materia = RegisterMaterias.objects.get(id=materia_id)
+            serializer = MateriasSerializer(materia)
+            return Response(serializer.data, status=200)
+
+        except RegisterMaterias.DoesNotExist:
+            return Response({'errors': 'invalid materia_id'}, status=404)
+
+    def delete(self, request, materia_id):
+        try:
+            materia = RegisterMaterias.objects.get(id=materia_id)
+            materia.delete()
+
+            return Response('', status=204)
+
+        except RegisterMaterias.DoesNotExist:
+            return Response({'errors': 'invalid materia_id'}, status=404)
+
+
 
 
 
