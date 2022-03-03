299ce35203947ecaf07619e8696e6c30cdbbfee0
Diogo <diogosantosferreira.01@gmail.com>
2022-03-03 19:58:03 -0300

    vaiiii

--- materias/models.py
+++ materias/models.py
@@ -1,6 +1,9 @@
 from django.db import models
- 
+from django.contrib.auth.models import User
+
+
 class RegisterMaterias(models.Model):
     name_materia = models.CharField(max_length=255, unique=True)
     qtd_aulas = models.IntegerField()
     professor = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- turmas/models.py
+++ turmas/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from materias.models import RegisterMaterias
+from django.contrib.auth.models import User
+
 
 class RegisterTurmas(models.Model):
     name_turma = models.CharField(max_length=255, unique=True)
     turno = models.CharField(max_length=255)
     materias = models.ManyToManyField(RegisterMaterias, related_name='turmas')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
